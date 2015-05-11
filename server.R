fd0c6a4392be857797a35dd64abc4e2a4b94e2e1
Juha Lehtiranta <jyatzy@gmail.com>
2015-05-11 22:55:45 +0300

    First commit

---
+++ server.R
@@ -0,0 +1,35 @@
+# server_dir <- dirname(sys.frame(1)$ofile)
+
+library(leaflet)
+
+shinyServer(function(input, output, session) {
+  
+  # create variable to ui
+  output$map_in_ui <- renderLeaflet({
+    # init map
+    leaflet() %>%
+      # add map layer
+      addTiles('//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png'
+               , attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>' ) %>% 
+      # set initial boundaries to centre of Helsinki
+      setView( lng=24.95 , lat=60.21 , zoom = 12) %>%
+      # add test circles
+      addCircleMarkers(lng = runif(10 , min = 24.9 , max = 25 )
+                       , lat = runif(10 , min = 60.20 , max = 60.22), 
+                       layerId = paste0("marker", 1:10))
+  }) 
+  
+  # example placeholder for texts
+    output$kotiosoite <- renderText({ paste( input$kotiosoite_from_ui ) })
+    output$muutto_osoite <- renderPrint({ cat(input$muutto_osoite_from_ui) })
+  # example placeholder for pictures  
+    output$koti_pic = renderPlot(plot(1:10))
+    output$muutto_pic = renderPlot(plot(10:1))
+   
+  observeEvent(input$map_in_ui_marker_click, {
+    leafletProxy("map_in_ui", session) %>% 
+      removeMarker(input$map_in_ui_marker_click$id
+      )
+  })
+  
+})
